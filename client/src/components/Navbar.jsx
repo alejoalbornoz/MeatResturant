a12aee703b83ba5b394bf550fe6c5cdb1755baa3
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-08-12 16:55:21 -0300

    Add: Home and Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-transparent z-30">
+      <div className="container mx-auto flex justify-between items-center py-4">
+        <h3 className="font-bold text-3xl italic">
+          <Link href="/">Don Grillado</Link>
+        </h3>
+        <ul className="flex gap-x-4 text-lg font-bold">
+          <li>
+            <Link href="/reservation">Reservas</Link>
+          </li>
+          <li>
+            <Link href="/menu">Menu</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contacto</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
